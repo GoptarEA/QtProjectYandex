b7be96af00c916be0ddce2835b0aeb7b01bb9355
GoptarEA <goptarea@gmail.com>
2023-03-12 17:39:35 +0300

    1

---
+++ db_functions.py
@@ -0,0 +1,75 @@
+import sqlite3
+import hashlib
+import os
+import datetime
+
+import base64
+
+
+
+users = sqlite3.connect('data/users.db')
+users_cur = users.cursor()
+
+works = sqlite3.connect('data/works.db')
+works_cur = works.cursor()
+
+
+def create_db():
+    users_cur.execute("""CREATE TABLE IF NOT EXISTS users(
+       userid INTEGER PRIMARY KEY AUTOINCREMENT,
+       login TEXT NOT NULL,
+       password TEXT NOT NULL);
+    """)
+    users.commit()
+
+    works_cur.execute("""CREATE TABLE IF NOT EXISTS works(
+           workid INTEGER PRIMARY KEY AUTOINCREMENT,
+           theme TEXT NOT NULL,
+           varcount INTEGER NOT NULL,
+           excount INTEGER NOT NULL,
+           creation_date TEXT NOT NULL);
+        """)
+    works.commit()
+
+
+def add_new_user(new_login, new_password):
+    s = base64.b64encode(new_password).decode('utf-8')
+    users_cur.execute(f"INSERT INTO users (login, password) VALUES ('{new_login}', '{s}');")
+    users.commit()
+
+
+def add_new_work(theme, varcount, excount):
+    works_cur.execute("INSERT INTO works VALUES(?, ?, ?, ?);", (theme, varcount, excount, datetime.date.today()))
+
+
+def check_user(login):
+    print(login)
+    users_cur.execute(f"SELECT * FROM users;")
+    users_list = users_cur.fetchall()
+    print(users_list)
+    return any([login in elem for elem in users_list])
+
+
+def get_user(login):
+    print(login)
+    users_cur.execute(f"SELECT * FROM users;")
+    users_list = users_cur.fetchall()
+    print('ok')
+    return [elem for elem in users_list if elem[1] == login]
+
+
+def hashUsersPassword(password, salt):
+    key = hashlib.pbkdf2_hmac(
+        'sha256',
+        password.encode('utf-8'),
+        salt,
+        100000,
+        dklen=128
+    )
+    return salt + key
+
+
+def check_password(db_password, input_password):
+    salt = base64.b64decode(db_password.encode("utf-8"))[:32]
+    print(db_password == str(base64.b64encode(hashUsersPassword(input_password, salt)), "utf-8"), sep="\n")
+    return db_password == str(base64.b64encode(hashUsersPassword(input_password, salt)), "utf-8")
---
+++ file_generator.py
@@ -0,0 +1,160 @@
+from docx import Document
+from docx.shared import Inches, Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from random import randint, sample
+from pprint import pprint
+import pymorphy2
+from docx2pdf import convert
+
+
+def system_translation(number, radixresult):
+    digits = "0123456789ABCDEF"
+    res = ''
+    number = abs(number)
+    while number != 0:
+        res = str(digits[number % radixresult]) + res
+        number //= radixresult
+    return res
+
+
+def generate_exercise(numbertasks, operation, radix):
+    res = [(system_translation(randint(20, 512), radix),
+           system_translation(randint(20, 512), radix)) for i in range(numbertasks)]
+    resanswers = [system_translation(eval(str(int(item[0], radix)) + operation +
+                       str(int(item[1], radix))), radix) for item in res]
+    return res, resanswers
+
+
+def generate_ariphmetic_pdf(theme, number_of_exercises, operations, radixs):
+    answers_document = Document()
+    answers_nextp = answers_document.add_paragraph()
+    answers_nextp.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    answers_nextrun = answers_nextp.add_run("Ответы")
+    answers_nextrun.font.size = Pt(16)
+    answers_nextrun.font.bold = True
+    answers_nextrun.font.name = 'Times New Roman'
+
+    document = Document()
+    nextp = document.add_paragraph()
+    nextp.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    nextrun = nextp.add_run("Cамостоятельная работа")
+    nextrun.font.size = Pt(16)
+    nextrun.font.bold = True
+    nextrun.font.name = 'Times New Roman'
+
+    nextp = document.add_paragraph()
+    nextp.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    nextrun = nextp.add_run(theme)
+    nextrun.font.size = Pt(16)
+    nextrun.font.bold = True
+    nextrun.font.name = 'Times New Roman'
+    points = 'абвгдежзиклмн'
+    for i in range(number_of_exercises):
+        nextp = document.add_paragraph()
+        nextp.alignment = WD_ALIGN_PARAGRAPH.LEFT
+        nextrun = nextp.add_run('№ ' + str(i + 1) + ". Выполните арифметические операции:")
+        nextrun.font.size = Pt(14)
+        nextrun.font.bold = False
+        nextrun.font.name = 'Times New Roman'
+
+        answers_nextp = answers_document.add_paragraph()
+        answers_nextp.alignment = WD_ALIGN_PARAGRAPH.LEFT
+        answers_nextrun = answers_nextp.add_run('№ ' + str(i + 1) + ".")
+        answers_nextrun.font.size = Pt(14)
+        answers_nextrun.font.bold = False
+        answers_nextrun.font.name = 'Times New Roman'
+        exercises = generate_exercise(6, operations[i % 3], radixs[i % 4])
+        for item, point in zip(exercises[0], points):
+            nextp = document.add_paragraph()
+            nextp.paragraph_format.first_line_indent = Inches(0.5)
+            nextp.alignment = WD_ALIGN_PARAGRAPH.LEFT
+            nextrun = nextp.add_run(point + ") " + item[0])
+            nextrun.subscript = False
+            nextrun.font.size = Pt(14)
+            nextrun.font.bold = False
+            nextrun.font.name = 'Times New Roman'
+            subnextrun = nextp.add_run(str(radixs[i % 4 ]))
+            subnextrun.subscript = True
+            nextrun = nextp.add_run(f" {operations[i % 3]} " + item[1])
+            subnextrun = nextp.add_run(str(radixs[i % 4]))
+            subnextrun.subscript = True
+            nextrun.font.size = Pt(14)
+            nextrun.font.bold = False
+            nextrun.font.name = 'Times New Roman'
+
+        for item, point in zip(exercises[1], points):
+            answers_nextp = answers_document.add_paragraph()
+            answers_nextp.paragraph_format.first_line_indent = Inches(0.5)
+            answers_nextp.alignment = WD_ALIGN_PARAGRAPH.LEFT
+            answers_nextrun = answers_nextp.add_run(point + ") " + item)
+            answers_nextrun.subscript = False
+            answers_nextrun.font.size = Pt(14)
+            answers_nextrun.font.bold = False
+            answers_nextrun.font.name = 'Times New Roman'
+            answers_subnextrun = answers_nextp.add_run(str(radixs[i % 4]))
+            answers_subnextrun.subscript = True
+
+    document.save('files/' + theme + '.docx')
+    convert('files/' + theme + '.docx')
+
+    answers_document.save('files/' + theme + " ОТВЕТЫ.docx")
+    convert('files/' + theme + " ОТВЕТЫ.docx")
+
+
+def generate_systems_pdf(number_of_exercises, radixfrom):
+    document = Document()
+    nextp = document.add_paragraph()
+    nextp.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    nextrun = nextp.add_run("Cамостоятельная работа")
+    nextrun.font.size = Pt(16)
+    nextrun.font.bold = True
+    nextrun.font.name = 'Times New Roman'
+
+    nextp = document.add_paragraph()
+    nextp.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    nextrun = nextp.add_run("Перевод между системами счисления")
+    nextrun.font.size = Pt(16)
+    nextrun.font.bold = True
+    nextrun.font.name = 'Times New Roman'
+    points = 'абвгдежзиклмн'
+
+    morph = pymorphy2.MorphAnalyzer()
+
+    d = {
+        "двоичная": 2,
+        "четверичная": 4,
+        "восьмеричная": 8,
+        "шестнадцатеричная": 16
+    }
+
+    for exer in range(number_of_exercises):
+        nextp = document.add_paragraph()
+        nextp.alignment = WD_ALIGN_PARAGRAPH.LEFT
+        systems = sample(radixfrom, 2)
+
+        word1 = morph.parse(systems[0])[0].inflect({"ADJF", "femn", "loct"})[0]
+        word2 = morph.parse(systems[1])[0].inflect({"ADJF", "femn", "accs"})[0]
+        nextrun = nextp.add_run('№ ' + str(exer + 1) + ". Выполните перевод из " +
+                                word1 + " в " + word2)
+        nextrun.font.size = Pt(14)
+        nextrun.font.bold = False
+        nextrun.font.name = 'Times New Roman'
+        exercises = [system_translation(randint(10, 1000), d[systems[0]]) for i in range(4)]
+        pprint(exercises)
+        for item, point in zip(exercises, points):
+            nextp = document.add_paragraph()
+            nextp.paragraph_format.first_line_indent = Inches(0.5)
+            nextp.alignment = WD_ALIGN_PARAGRAPH.LEFT
+            nextrun = nextp.add_run(point + ") " + item)
+            nextrun.subscript = False
+            nextrun.font.size = Pt(14)
+            nextrun.font.bold = False
+            nextrun.font.name = 'Times New Roman'
+            subnextrun = nextp.add_run(str(d[systems[0]]))
+            subnextrun.subscript = True
+            nextrun.font.size = Pt(14)
+            nextrun.font.bold = False
+            nextrun.font.name = 'Times New Roman'
+
+    document.save('files/СР Перевод между системами счисления.docx')
+    convert('files/СР Перевод между системами счисления.docx')
